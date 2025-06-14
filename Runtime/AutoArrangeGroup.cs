73f500d032f9b5feebb57edf24ac9fc030016f9c
Enwongo-Abasi Mfon Udomah <promzy132@gmail.com>
2025-06-14 07:29:24 +0100

    fixed script bugs

--- Runtime/AutoArrangeGroup.cs
+++ Runtime/AutoArrangeGroup.cs
@@ -1,42 +1,45 @@
 using UnityEngine;
 
-[ExecuteAlways]
-public class AutoArrangeGroup : MonoBehaviour
+namespace AAG
 {
-    public enum LayoutAxis { X, Y, Z }
-    public LayoutAxis layoutAxis = LayoutAxis.X;
-
-    public float spacing = 1f;
-    public Vector3 padding = Vector3.zero;
-    public bool autoUpdate = true;
-
-    private void OnValidate()
+    [ExecuteAlways]
+    public class AutoArrangeGroup : MonoBehaviour
     {
-        if (autoUpdate)
-            RebuildLayout();
-    }
+        public enum LayoutAxis { X, Y, Z }
+        public LayoutAxis layoutAxis = LayoutAxis.X;
 
-    [ContextMenu("Rebuild Layout")]
-    public void RebuildLayout()
-    {
-        Vector3 offset = padding;
+        public float spacing = 1f;
+        public Vector3 padding = Vector3.zero;
+        public bool autoUpdate = true;
 
-        for (int i = 0; i < transform.childCount; i++)
+        private void OnValidate()
         {
-            Transform child = transform.GetChild(i);
-            child.localPosition = offset;
+            if (autoUpdate)
+                RebuildLayout();
+        }
 
-            switch (layoutAxis)
+        [ContextMenu("Rebuild Layout")]
+        public void RebuildLayout()
+        {
+            Vector3 offset = padding;
+
+            for (int i = 0; i < transform.childCount; i++)
             {
-                case LayoutAxis.X:
-                    offset += new Vector3(spacing, 0f, 0f);
-                    break;
-                case LayoutAxis.Y:
-                    offset += new Vector3(0f, spacing, 0f);
-                    break;
-                case LayoutAxis.Z:
-                    offset += new Vector3(0f, 0f, spacing);
-                    break;
+                Transform child = transform.GetChild(i);
+                child.localPosition = offset;
+
+                switch (layoutAxis)
+                {
+                    case LayoutAxis.X:
+                        offset += new Vector3(spacing, 0f, 0f);
+                        break;
+                    case LayoutAxis.Y:
+                        offset += new Vector3(0f, spacing, 0f);
+                        break;
+                    case LayoutAxis.Z:
+                        offset += new Vector3(0f, 0f, spacing);
+                        break;
+                }
             }
         }
     }
